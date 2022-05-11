6f634bfbc41a26e884270d110ecc00615e3bb226
half-winter-moon <han16064@byui.edu>
2022-05-11 09:20:01 -0600

    updated code with week 4 swagger documentation

--- routes/contacts.js
+++ routes/contacts.js
@@ -6,11 +6,8 @@ const contactsController = require('../controllers/contacts');
 router.get('/', contactsController.getData);
 
 router.get('/:id', contactsController.getPeiceOfData);
-
-router.post('/', contactsController.createContact);
-
+router.put('/:id', contactsController.createContact);
 router.put('/:id', contactsController.updateContact);
-
 router.delete('/:id', contactsController.deleteContact);
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const contactInfo = require('../controllers/contacts.js');
 
-// // GET all docs from contacts collection
-// router.get('/', contactInfo.getData);
-
-// // GET a single doc from contacts collection where id matches
-// router.get('/:id', contactInfo.getPeiceOfData);
-
-// use contacts.js file to make multiple routes
-router.use('/contacts', require('./contacts'));
+router.use('/contacts', require('./contacts'))
 
 module.exports = router;
